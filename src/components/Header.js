0eb9859e3fb6105a0bfabade4b2f6a4ac20f1437
EASYMAK777 <jplocallawncare@gmail.com>
2020-09-28 19:36:35 -0400

    created search form

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Navbar from "react-bootstrap/Navbar";
 
-export default function Nav() {
+export default function Header() {
     return(
         <Navbar bg="dark" variant="dark">
             <Navbar.Brand>React Employee Tracker</Navbar.Brand>        
