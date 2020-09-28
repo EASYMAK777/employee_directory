11c4c83a23b50a603a86f983a1bd867951e48d48
EASYMAK777 <jplocallawncare@gmail.com>
2020-09-28 19:32:44 -0400

    created header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,37 +1,10 @@
-import React from 'react';
-import { Container, Row, Navbar, Nav, NavDropdown, Form, FormControl, Button, Col, Table } from 'react-bootstrap';
+import React from "react";
+import Navbar from "react-bootstrap/Navbar";
 
-function Header() {
-    return (
-        <div className="Navbar">
-            <header className="Navbar-Header">
-                <Container fluid='xl'>
-                    <Row>
-                        <Col>
-                            <Navbar bg="light" expand="lg">
-                                <Navbar.Brand href="#home">Greed Corp Employee Directory</Navbar.Brand>
-                                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                                <Navbar.Collapse id="basic-navbar-nav">
-                                    <Nav className="mr-auto">
-                                        <NavDropdown title="Sort By" id="basic-nav-dropdown">
-                                            <NavDropdown.Item href="#action/3.1">By Name</NavDropdown.Item>
-                                            <NavDropdown.Item href="#action/3.2">By Id</NavDropdown.Item>
-                                            <NavDropdown.Divider />
-                                        </NavDropdown>
-                                    </Nav>
-                                    <Form inline>
-                                        <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-                                        <Button variant="outline-success">Search</Button>
-                                    </Form>
-                                </Navbar.Collapse>
-                            </Navbar>
-                        </Col>
-                    </Row>
-                </Container>
-
-            </header>
-        </div>
+export default function Nav() {
+    return(
+        <Navbar bg="dark" variant="dark">
+            <Navbar.Brand>React Employee Tracker</Navbar.Brand>        
+        </Navbar>
     )
-}
-
-export default Header; 
+}; 
